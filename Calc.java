461dd47f5a21024f6c5602287cc1a68c912cda4e
Selassie <queensjng@gmail.com>
2017-01-22 20:39:11 +0000

    Add working

---
+++ Calc.java
@@ -0,0 +1,12 @@
+//Selassie Golloh
+
+import java.util.Scanner;
+public class Calc{
+  public static void main(String[] args){
+    if (parameters[0].equals("add")){
+      double one = Double.parseDouble(parameters[1]);
+      double two = Double.parseDouble(parameters[2]);
+      System.out.println(one + two);
+    }
+  }
+}
