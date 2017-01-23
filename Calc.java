ed762365826a16935c89e6d39f00b7e3712c90b4
Selassie <queensjng@gmail.com>
2017-01-23 10:30:26 +0000

    I have committed add in master

--- Calc.java
+++ Calc.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 public class Calc{
-  public static void main(String[] args){
+  public static void main(String[] parameters){
     if (parameters[0].equals("add")){
       double one = Double.parseDouble(parameters[1]);
       double two = Double.parseDouble(parameters[2]);
