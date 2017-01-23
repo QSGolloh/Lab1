208bab0ec8b50daafb12b1f0733ea549679e070a
Selassie <queensjng@gmail.com>
2017-01-23 10:32:26 +0000

    I have committed multiply

--- Calc.java
+++ Calc.java
@@ -8,5 +8,10 @@ public class Calc{
       double two = Double.parseDouble(parameters[2]);
       System.out.println(one + two);
     }
+    if (parameters[0].equals("multiply")){
+      double one = Double.parseDouble(parameters[1]);
+      double two = Double.parseDouble(parameters[2]);
+      System.out.println(one * two);
+    }
   }
 }
